c2db50ea533ab047054e8731b29500742c006ab8
Gihwan Kim <ghkim3221@gmail.com>
2019-06-23 18:17:28 +0900

    Add middle big endian to the EndiannessTypography component

--- src/component/EndiannessTypography/EndiannessTypography.js
+++ src/component/EndiannessTypography/EndiannessTypography.js
@@ -5,17 +5,37 @@ import Typography from '@material-ui/core/Typography';
 
 function EndiannessTypography(props) {
   const { endianness } = props;
-  const isBigEndian = (endianness.toLowerCase() === 'big');
 
-  return (
-    <Typography color="textPrimary" variant="h5">
-      {isBigEndian ? 'Big' : 'Little'} Endian
-    </Typography>
-  );
+  switch (endianness) {
+    case 'big':
+      return (
+        <Typography color="textPrimary" variant="h5">
+          Big Endian
+        </Typography>
+      );
+    case 'middleBig':
+      return (
+        <Typography color="textPrimary" variant="h5">
+          Middle Big Endian
+        </Typography>
+      );
+    case 'little':
+      return (
+        <Typography color="textPrimary" variant="h5">
+          Little Endian
+        </Typography>
+      );
+    default:
+      return (
+        <Typography color="textPrimary" variant="h5">
+          Unknown
+        </Typography>
+      );
+  }
 }
 
 EndiannessTypography.propTypes = {
-  endianness: PropTypes.oneOf(['big', 'little']).isRequired,
+  endianness: PropTypes.oneOf(['big', 'middleBig', 'little']).isRequired,
 };
 
 export default EndiannessTypography;

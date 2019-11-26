a411b21578cd72f86ed0a5d5375582850d4fc4a0
Gihwan Kim <ghkim3221@gmail.com>
2019-11-26 14:29:55 +0900

    Fix eslint problems for Float

--- src/components/Float.jsx
+++ src/components/Float.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 
 import FloatTypography from './FloatTypography';
 import HexStringTypography from './HexStringTypography';
 
-const Float = ({ array, byteLength }) => {
-  return (
-    <Grid spacing={16} container>
-      <Grid xs={12} item>
-        <HexStringTypography array={array} />
-      </Grid>
-      <Grid xs={12} item>
-        <FloatTypography array={array} byteLength={byteLength} />
-      </Grid>
+const Float = ({ array, byteLength }) => (
+  <Grid spacing={16} container>
+    <Grid xs={12} item>
+      <HexStringTypography array={array} />
     </Grid>
-  );
-}
+    <Grid xs={12} item>
+      <FloatTypography array={array} byteLength={byteLength} />
+    </Grid>
+  </Grid>
+);
 
 Float.propTypes = {
-  array: PropTypes.array.isRequired,
+  array: PropTypes.arrayOf(PropTypes.number).isRequired,
   byteLength: PropTypes.oneOf([4, 8]).isRequired,
 };
 

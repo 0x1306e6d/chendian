3bf48cc2e19f4f88ad0ebeb06554fefc9e67a48b
Gihwan Kim <ghkim3221@gmail.com>
2019-11-26 16:08:35 +0900

    Deploy the version 1.2.0

---
+++ precache-manifest.41b79818b55757f99ee4ac3bbc023b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6160e90a02b0d62d74cc9a22b5cb892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2909d03d0347b66c953",
+    "url": "/static/css/main.d81ec14a.chunk.css"
+  },
+  {
+    "revision": "a2041e2445920c81f072",
+    "url": "/static/js/2.ee8bf284.chunk.js"
+  },
+  {
+    "revision": "e2909d03d0347b66c953",
+    "url": "/static/js/main.a507ec06.chunk.js"
+  },
+  {
+    "revision": "94c339046e7134804820",
+    "url": "/static/js/runtime-main.bc2d5e8c.js"
+  }
+]);

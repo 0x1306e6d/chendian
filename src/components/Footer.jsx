9611fbca8ba1c799064b57afe58990dfc8868d55
Gihwan Kim <ghkim3221@gmail.com>
2019-12-10 21:01:30 +0900

    Apply styles using Hook API for the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4),
   },
@@ -12,35 +12,35 @@ const styles = (theme) => ({
     fontFamily:
       'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
   },
-});
+}));
 
-const Footer = ({ classes, version }) => (
-  <footer className={classes.root}>
-    <Typography
-      className={classes.typography}
-      align="center"
-      color="textPrimary"
-      variant="subtitle1"
-    >
-      {version}
-    </Typography>
-    <Typography
-      className={classes.typography}
-      align="center"
-      color="textPrimary"
-      variant="subtitle1"
-    >
-      Made by Gihwan Kim, Powered by GitHub Pages
-    </Typography>
-  </footer>
-);
+const Footer = ({ version }) => {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.root}>
+      <Typography
+        className={classes.typography}
+        align="center"
+        color="textPrimary"
+        variant="subtitle1"
+      >
+        {version}
+      </Typography>
+      <Typography
+        className={classes.typography}
+        align="center"
+        color="textPrimary"
+        variant="subtitle1"
+      >
+        Made by Gihwan Kim, Powered by GitHub Pages
+      </Typography>
+    </footer>
+  );
+};
 
 Footer.propTypes = {
-  classes: PropTypes.shape({
-    root: PropTypes.string.isRequired,
-    typography: PropTypes.string.isRequired,
-  }).isRequired,
   version: PropTypes.string.isRequired,
 };
 
-export default withStyles(styles)(Footer);
+export default Footer;

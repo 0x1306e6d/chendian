803972e58437183aaed68860c6c1a08f4f6c12b8
Gihwan Kim <ghkim3221@gmail.com>
2019-12-10 21:05:13 +0900

    Apply styles using Hook API for the NavigationBrand component

--- src/components/NavigationBrand.jsx
+++ src/components/NavigationBrand.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import withStyles from '@material-ui/core/styles/withStyles';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     textTransform: 'lowercase',
   },
   icon: {
     marginRight: theme.spacing(1),
   },
-});
+}));
 
-const NavigationBrand = ({ classes }) => (
-  <Button className={classes.root} color="inherit" component="a" href="/">
-    <ShuffleIcon className={classes.icon} color="inherit" />
-    <Typography color="inherit" component="h1" variant="h6">
-      chendian
-    </Typography>
-  </Button>
-);
+const NavigationBrand = () => {
+  const classes = useStyles();
 
-NavigationBrand.propTypes = {
-  classes: PropTypes.shape({
-    root: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  }).isRequired,
+  return (
+    <Button className={classes.root} color="inherit" component="a" href="/">
+      <ShuffleIcon className={classes.icon} color="inherit" />
+      <Typography color="inherit" component="h1" variant="h6">
+        chendian
+      </Typography>
+    </Button>
+  );
 };
 
-export default withStyles(styles)(NavigationBrand);
+export default NavigationBrand;

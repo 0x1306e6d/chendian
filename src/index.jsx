45b975a5ebd7e7ce5cfb3ce0ec3693cedd66ac90
Gihwan Kim <ghkim3221@gmail.com>
2019-12-19 21:11:52 +0900

    Remove App.css

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './App';
-import './App.css';
 
 import reducer from './reducer';
 import * as serviceWorker from './serviceWorker';
